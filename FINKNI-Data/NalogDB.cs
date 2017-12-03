ae51bf6f1b2a4712d4dcca464702b8e0648f389e
dimit <dimit@DESKTOP-OVICMOB>
2017-12-03 14:53:40 +0100

    Kreiram DB za Nalog

--- FINKNI-Data/NalogDB.cs
+++ FINKNI-Data/NalogDB.cs
@@ -14,7 +14,49 @@ namespace FINKNI_Data
         {
             List<Nalog> nlist = new List<Nalog>();
             SqlConnection connection = AppDB.GetConnection();
+            SqlCommand selectAllCommand = new SqlCommand();
+            selectAllCommand.Connection = connection;
+            selectAllCommand.CommandText = "DnevnikNaloziSelectAll";
+            selectAllCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = selectAllCommand.ExecuteReader();
+                while(reader.Read())
+                {
+                    Nalog nalog = new Nalog();
+                    nalog.BrojNaNalog = reader["BrojNaNalog"].ToString();
+                    nalog.DatumNaNaNalog = Convert.ToDateTime(reader["DatumNaNalog"]);
+                    nalog.SaldoDolzi = Convert.ToDecimal(reader["SaldoDolzi"]);
+                    nalog.SaldoPobaruva = Convert.ToDecimal(reader["SaldoPobaruva"]);
+                    nlist.Add(nalog);
+                }
+                reader.Close();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return nlist;
         }
+
+
+        public static Nalog GetNalog()
+        {
+            Nalog nalog = new Nalog();
+            SqlConnection connection = AppDB.GetConnection();
+            SqlCommand selectCommand = new SqlCommand();
+            selectCommand.Connection = connection;
+            selectCommand.CommandText = "";
+            selectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+            selectCommand.Parameters.AddWithValue()
+        }
+
 
     }
 }
